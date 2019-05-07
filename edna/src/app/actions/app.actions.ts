d875261272cde979a4d50f8dc79c2f30a685e151
Itay Yaffe <itayy@codevalue.net>
2019-05-07 11:18:15 +0300

    commit-20190507_1118

--- edna/src/app/actions/app.actions.ts
+++
@@ -1,9 +0,0 @@
-export class SetUserName {
-  static readonly type = "[App] Set Username";
-  constructor(public payload: string) {}
-}
-
-export class SetCourseName {
-  static readonly type = "[TUTORIAL] Set Course Name";
-  constructor(public payload: string) {}
-}
